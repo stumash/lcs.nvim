5945ca7e503ade347637184ec62952e24e25aa6c
Stuart Mashaal <stuart.mashaal@gmail.com>
2022-04-29 01:22:59 -0400

    untested but complete

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # lcs.nvim
-a simple plugin to manage your lcs (listchars) settings
+
+Manage your `listchars` display settings easily.
+
+# Installation
+
+`Plug 'stumash/lcs.nvim'`
+
+I use plug, adapt the above line as needed for you plugin manager of choice.
+
+# Usage
+
+```lua
+-- these are the defaults, you can omit any of these settings
+require"lcs.nvim".setup{
+  enabled = true,
+  chars = {
+    eol = { enabled = true, value = [[eol:↴]] },
+    tab = { enabled = true, value = [[tab:→\ ]] },
+    spc = { enabled = false, value = [[space:·]] },
+    trail = { enabled = true, value = [[space:-]] },
+  },
+}
+```
+
+```vim
+nnoremap <leader>LCSs <CMD>lua toggleShowSpace()<CR>
+nnoremap <leader>LCSt <CMD>lua toggleShowTab()<CR>
+nnoremap <leader>LCSe <CMD>lua toggleShowEol()<CR>
+nnoremap <leader>LCSr <CMD>lua toggleShowTrail()<CR>
+" toggle listchar visibility
+nnoremap <leader>LCSL <CMD>lua toggleListchars()<CR>
+```
---
+++ lua/lcs.lua
@@ -0,0 +1,77 @@
+local M = {}
+
+local lcs_settings = {}
+local default_lcs_settings = {
+  enabled = true,
+  chars = {
+    eol = { enabled = true, value = [[eol:↴]] },
+    tab = { enabled = true, value = [[tab:→\ ]] },
+    spc = { enabled = false, value = [[space:·]] },
+    trail = { enabled = true, value = [[space:-]] },
+  },
+}
+
+local function setLcs()
+  local lcs = ''
+
+  local function addToLcs(s)
+    if lcs == '' then
+      lcs = s
+    else
+      lcs = lcs .. ',' .. s
+    end
+  end
+
+  for _, setting in pairs(lcs_settings.chars) do
+    if setting.enabled then
+      addToLcs(setting.value)
+    end
+  end
+  vim.o.lcs = lcs
+
+  if lcs_settings.enabled then
+    vim.o.list = true
+  else
+    vim.o.list = false
+  end
+end
+
+function M.toggleShowSpace()
+  lcs_settings.chars.spc.enabled = not lcs_settings.chars.spc.enabled
+  setLcs()
+end
+function M.toggleShowTab()
+  lcs_settings.chars.tab.enabled = not lcs_settings.chars.tab.enabled
+  setLcs()
+end
+function M.toggleShowEol()
+  lcs_settings.chars.eol.enabled = not lcs_settings.chars.eol.enabled
+  setLcs()
+end
+function M.toggleShowTrail()
+  lcs_settings.chars.trail.enabled = not lcs_settings.chars.trail.enabled
+  setLcs()
+end
+function M.toggleListchars()
+  lcs_settings.enabled = not lcs_settings.enabled
+  setLcs()
+end
+
+
+function M.setup(user_lcs_settings)
+  -- copy defaults
+  lcs_settings.enabled = default_lcs_settings.enabled
+  lcs_settings.chars = vim.deepcopy(default_lcs_settings.chars)
+
+  -- apply user settings
+  if user_lcs_settings.enabled ~= nil then
+    lcs_settings.enabled = user_lcs_settings.enabled
+  end
+  if user_lcs_settings.chars ~= nil then
+    lcs_settings.chars = vim.deepcopy(user_lcs_settings.chars)
+  end
+
+  setLcs()
+end
+
+return M
